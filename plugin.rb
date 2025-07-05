a4fdd3f3dbaa797ce28eb64aece170b530c74c4c
Wandering Consciousness <10329002+Wandering-Consciousness@users.noreply.github.com>
2025-07-05 14:36:39 +1200

    remove JS asset loading config

--- plugin.rb
+++ plugin.rb
@@ -8,12 +8,6 @@ enabled_site_setting :discourse_url_copier_enabled
 
 register_asset "stylesheets/discourse-url-copier.scss"
 
-after_initialize do
-  # Add the plugin's JavaScript assets
-  register_asset "javascripts/discourse/components/url-copier-button.js"
-  register_asset "javascripts/discourse/connectors/topic-above-post-stream/url-copier-button.js"
-end
-
 # Register site settings
 
 # Site settings configuration
