6e82cf9a2b5269578b89d115f702a7385328e691
Wandering Consciousness <10329002+Wandering-Consciousness@users.noreply.github.com>
2025-07-05 15:08:45 +1200

    build project from skeleton using rake

--- plugin.rb
+++ plugin.rb
@@ -1,13 +1,21 @@
+# frozen_string_literal: true
+
 # name: discourse-url-copier
 # about: Adds a button to copy modified URLs from topics in specified categories
+# meta_topic_id: TODO
 # version: 1.0.0
 # authors: Project Zenki
 # url: https://github.com/q-cognition/discourse-url-copier
+# required_version: 2.7.0
 
 enabled_site_setting :discourse_url_copier_enabled
 
-register_asset "stylesheets/discourse-url-copier.scss"
+module ::MyPluginModule
+  PLUGIN_NAME = "discourse-url-copier"
+end
 
-# Register site settings
+require_relative "lib/my_plugin_module/engine"
 
-# Site settings configuration
+after_initialize do
+  # Code which should run after Rails has finished booting
+end
